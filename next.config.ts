988862dbe2b649d5f7b6419280b5d72e6edec292
Rodel <rodel.crisosto7@gmail.com>
2025-05-31 15:50:43 +0800

    refactor: remove unused getBaseUrl import and call in HomePage component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    env: {
+        OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
+        FORSQUARE_API_KEY: process.env.FORSQUARE_API_KEY,
+    },
 };
 
 export default nextConfig;
