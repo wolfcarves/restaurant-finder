8a5bfaa6522aac80c0e4c3acf923d94ab39d0c6c
Rodel <rodel.crisosto7@gmail.com>
2025-05-31 15:36:14 +0800

    fix: correct base URL logic for development and production environments

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,3 +1,3 @@
 export const getBaseUrl = () => {
-    return process.env.NODE_ENV !== 'production' ? 'localhost:3000' : process.env.API_BASE_URL;
+    return process.env.NODE_ENV === 'development' ? 'localhost:3000' : process.env.NEXT_API_BASE_URL;
 };
